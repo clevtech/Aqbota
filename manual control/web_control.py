90fc43c10b44c670b49b53167679bb806a7d7826
BiggyBaron <dr.cleverest@gmail.com>
2019-05-30 13:57:04 +0600

    Uncomment ROS part

--- manual control/web_control.py
+++ manual control/web_control.py
@@ -13,115 +13,93 @@ from flask import Flask, render_template, request, Markup, jsonify
 
 
 app = Flask(__name__)  # Creating new flask app
-# settings, pub, speed, turn, x, y, z, th, status, moveBindings = init_motion()
+settings, pub, speed, turn, x, y, z, th, status, moveBindings = init_motion()
 
-# def init_motion():
-#     settings = termios.tcgetattr(sys.stdin)
-#
-#     pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-#     rospy.init_node('teleop_twist_keyboard')
-#
-#     speed = rospy.get_param("~speed", 0.5)
-#     turn = rospy.get_param("~turn", 1.0)
-#     x = 0
-#     y = 0
-#     z = 0
-#     th = 0
-#     status = 0
-#
-#     moveBindings = {
-#         'i': (1, 0, 0, 0),
-#         'o': (1, 0, 0, -1),
-#         'j': (0, 0, 0, 1),
-#         'l': (0, 0, 0, -1),
-#         'u': (1, 0, 0, 1),
-#         ',': (-1, 0, 0, 0),
-#         '.': (-1, 0, 0, 1),
-#         'm': (-1, 0, 0, -1),
-#         'O': (1, -1, 0, 0),
-#         'I': (1, 0, 0, 0),
-#         'J': (0, 1, 0, 0),
-#         'L': (0, -1, 0, 0),
-#         'U': (1, 1, 0, 0),
-#         '<': (-1, 0, 0, 0),
-#         '>': (-1, -1, 0, 0),
-#         'M': (-1, 1, 0, 0),
-#         't': (0, 0, 1, 0),
-#         'b': (0, 0, -1, 0),
-#     }
-#
-#     return settings, pub, speed, turn, x, y, z, th, status, moveBindings
+def init_motion():
+    settings = termios.tcgetattr(sys.stdin)
+
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    rospy.init_node('teleop_twist_keyboard')
+
+    speed = rospy.get_param("~speed", 0.5)
+    turn = rospy.get_param("~turn", 1.0)
+    x = 0
+    y = 0
+    z = 0
+    th = 0
+    status = 0
+
+    moveBindings = {
+        'i': (1, 0, 0, 0),
+        'o': (1, 0, 0, -1),
+        'j': (0, 0, 0, 1),
+        'l': (0, 0, 0, -1),
+        'u': (1, 0, 0, 1),
+        ',': (-1, 0, 0, 0),
+        '.': (-1, 0, 0, 1),
+        'm': (-1, 0, 0, -1),
+        'O': (1, -1, 0, 0),
+        'I': (1, 0, 0, 0),
+        'J': (0, 1, 0, 0),
+        'L': (0, -1, 0, 0),
+        'U': (1, 1, 0, 0),
+        '<': (-1, 0, 0, 0),
+        '>': (-1, -1, 0, 0),
+        'M': (-1, 1, 0, 0),
+        't': (0, 0, 1, 0),
+        'b': (0, 0, -1, 0),
+    }
+
+    return settings, pub, speed, turn, x, y, z, th, status, moveBindings
 
 
 def motion(key):
     print(key)
-#
-#     moveBindings = {
-#             'i': (1, 0, 0, 0),
-#             'o': (1, 0, 0, -1),
-#             'j': (0, 0, 0, 1),
-#             'l': (0, 0, 0, -1),
-#             'u': (1, 0, 0, 1),
-#             ',': (-1, 0, 0, 0),
-#             '.': (-1, 0, 0, 1),
-#             'm': (-1, 0, 0, -1),
-#             'O': (1, -1, 0, 0),
-#             'I': (1, 0, 0, 0),
-#             'J': (0, 1, 0, 0),
-#             'L': (0, -1, 0, 0),
-#             'U': (1, 1, 0, 0),
-#             '<': (-1, 0, 0, 0),
-#             '>': (-1, -1, 0, 0),
-#             'M': (-1, 1, 0, 0),
-#             't': (0, 0, 1, 0),
-#             'b': (0, 0, -1, 0),
-#         }
-#
-#     try:
-#         if key in moveBindings.keys():
-#             x = moveBindings[key][0]
-#             y = moveBindings[key][1]
-#             z = moveBindings[key][2]
-#             th = moveBindings[key][3]
-#         elif key in speedBindings.keys():
-#             speed = speed * speedBindings[key][0]
-#             turn = turn * speedBindings[key][1]
-#
-#             print(vels(speed, turn))
-#             if (status == 14):
-#                 print(msg)
-#             status = (status + 1) % 15
-#         else:
-#             x = 0
-#             y = 0
-#             z = 0
-#             th = 0
-#             if (key == '\x03'):
-#                 break
-#
-#         twist = Twist()
-#         twist.linear.x = x * speed;
-#         twist.linear.y = y * speed;
-#         twist.linear.z = z * speed;
-#         twist.angular.x = 0;
-#         twist.angular.y = 0;
-#         twist.angular.z = th * turn
-#         pub.publish(twist)
-#
-#     except Exception as e:
-#         print(e)
-#
-#     finally:
-#         twist = Twist()
-#         twist.linear.x = 0;
-#         twist.linear.y = 0;
-#         twist.linear.z = 0
-#         twist.angular.x = 0;
-#         twist.angular.y = 0;
-#         twist.angular.z = 0
-#         pub.publish(twist)
-#
-#         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    try:
+        if key in moveBindings.keys():
+            x = moveBindings[key][0]
+            y = moveBindings[key][1]
+            z = moveBindings[key][2]
+            th = moveBindings[key][3]
+        elif key in speedBindings.keys():
+            speed = speed * speedBindings[key][0]
+            turn = turn * speedBindings[key][1]
+
+            print(vels(speed, turn))
+            if (status == 14):
+                print(msg)
+            status = (status + 1) % 15
+        else:
+            x = 0
+            y = 0
+            z = 0
+            th = 0
+            if (key == '\x03'):
+                break
+
+        twist = Twist()
+        twist.linear.x = x * speed;
+        twist.linear.y = y * speed;
+        twist.linear.z = z * speed;
+        twist.angular.x = 0;
+        twist.angular.y = 0;
+        twist.angular.z = th * turn
+        pub.publish(twist)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        twist = Twist()
+        twist.linear.x = 0;
+        twist.linear.y = 0;
+        twist.linear.z = 0
+        twist.angular.x = 0;
+        twist.angular.y = 0;
+        twist.angular.z = 0
+        pub.publish(twist)
+
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 
 
 @app.route('/robot-control/')
