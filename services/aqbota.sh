83e87c6231d0e80b40e3a7c85269a1ce22c3d6a5
BiggyBaron <dr.cleverest@gmail.com>
2019-06-05 20:44:29 +0600

    Try my best

--- services/aqbota.sh
+++ services/aqbota.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-bash -c "script /dev/null"
+#bash -c "script /dev/null"
 bash -c "source ~/.bashrc"
 bash -c "cd ~/catkin_ws/"
-bash -c "source /opt/ros/kinetic/setup.bash"
-bash -c "source ~/catkin_ws/devel/setup.bash"
-bash -c "source /home/robot/linorobot_ws/devel/setup.bash"
+bash -c "/opt/ros/kinetic/setup.sh"
+bash -c "/home/robot/linorobot_ws/devel/setup.sh"
 bash -c "export LINOLIDAR=rplidar"
 bash -c "export LINOBASE=2wd"
 bash -c "/opt/ros/kinetic/bin/catkin_make"
