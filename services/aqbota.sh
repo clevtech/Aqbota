96e40a74210be398f9dce36ec06bf10806cad1b5
BiggyBaron <dr.cleverest@gmail.com>
2019-06-05 19:51:12 +0600

    Try catkin make

--- services/aqbota.sh
+++ services/aqbota.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-bash -c "source /home/robot/catkin_ws/devel/setup.bash && roslaunch linorobot aqbota.launch"
+bash -c "source /home/robot/catkin_ws/devel/setup.bash"
+bash -c "catkin_make"
+bash -c "roslaunch linorobot aqbota.launch"
