cdbc07b5b1bba86c913ded097089cf3f89e99d50
BiggyBaron <dr.cleverest@gmail.com>
2019-06-05 20:00:18 +0600

    Env. variable linoradar source

--- services/aqbota.sh
+++ services/aqbota.sh
@@ -3,5 +3,7 @@
 bash -c "source /opt/ros/kinetic/setup.bash"
 bash -c "source ~/catkin_ws/devel/setup.bash"
 bash -c "source /home/robot/linorobot_ws/devel/setup.bash"
+bash -c "export LINOLIDAR=rplidar"
+bash -c "export LINOBASE=2wd"
 bash -c "catkin_make"
 bash -c "roslaunch linorobot aqbota.launch"
