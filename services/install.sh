bac649609380694cbcdea951a6845db0a8106c49
BiggyBaron <dr.cleverest@gmail.com>
2019-06-05 19:19:23 +0600

    Chmodding

--- services/install.sh
+++ services/install.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-cp ./aqbota.service /lib/systemd/system/aqbota.service
 cp ./aqbota.sh /home/robot/aqbota.sh
 sudo chmod 777 /home/robot/aqbota.sh
+echo "Aqbota script is created"
+
+cp ./aqbota.service /lib/systemd/system/aqbota.service
+echo "Aqbota service is created"
 
 sudo service aqbota start
+echo "Aqbota service is started"
 sudo service aqbota enable
 
 echo "Aqbota is installed"
