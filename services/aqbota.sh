a24ee9250e04787995d0125972b38c6e84ae4d49
BiggyBaron <dr.cleverest@gmail.com>
2019-06-05 19:25:14 +0600

    catkin debug

--- services/aqbota.sh
+++ services/aqbota.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bash -c "cd catkin_ws && catkin_make && source /home/robot/catkin_ws/devel/setup.bash && roslaunch linorobot aqbota.launch"
+bash -c "cd ~/catkin_ws && catkin_make && source /home/robot/catkin_ws/devel/setup.bash && roslaunch linorobot aqbota.launch"
