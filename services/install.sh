7a8e3983afa8a14d82a672248cf42d6cbeeb7f01
BiggyBaron <dr.cleverest@gmail.com>
2019-06-05 19:14:49 +0600

    Chmodding

--- services/install.sh
+++ services/install.sh
@@ -2,6 +2,7 @@
 
 cp ./aqbota.service /lib/systemd/system/aqbota.service
 cp ./aqbota.sh /home/robot/aqbota.sh
+sudo chmod 777 /home/robot/aqbota.sh
 
 sudo service aqbota start
 sudo service aqbota enable
